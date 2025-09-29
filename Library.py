9bf8b163e5d0dda74219aa1bc83c28b1d4fd723b
Maxym <hako.diaha@gmail.com>
2025-09-29 22:14:08 +0200

    Зробив нормальний вивід інфи

--- Library.py
+++ Library.py
@@ -1,22 +1,28 @@
 class Library():
     def __init__(self):
-        self.books = []
+        self._books = []
         self.users = []
 
-    def add_books(self, book):
-        self.books.append(book)
+    def add_book(self, book):
+        self._books.append(book)
 
-    def add_users(self, user):
+    def add_user(self, user):
         self.users.append(user)
 
     def find_book(self):
-        pass
+        pass 
 
-    def show_all_Books(books):
-            print(books)    
-
-    def issue_book_to_user(self):
-        pass
+    def issue_book_to_user(self, user_id, book_id):
+        self.user_id = user_id
+        self.book_id = book_id
+        user = None 
+        for i in self.users, range(1, 3):
+            if i == user_id:
+                for j in self._books:
+                    if j == book_id:
+                        print("I found it!")
+            else:
+                print("I didnt find it..")    
 
     def return_book_from_user(self):
         pass
@@ -24,13 +30,20 @@ class Library():
 
 
 class User():
-    def __init__(self, name, lastname, user_id):
+    def __init__(self, name, lastname, user_id, borrowed_books):
         self.name = name
         self.lastname = lastname
-        self.id = user_id
+        self.user_id = user_id
+        self.borrowed_books = []
 
     def check_borrowed_books(self):
-        print(f"Borrowed books: {self.borrowed_books}") 
+        print(f"Borrowed books: {self.borrowed_books}")
+
+    def __repr__(self):
+        return f"{self.name!r} | {self.lastname!r} | {self.user_id}"
+    
+    def borrow_book(self):
+        pass
 
 
 
@@ -43,44 +56,63 @@ class Book():
         self.genre = genre
         self.is_available = is_available
 
-    def show_info(self):
-        print(f"{self.name} | {self.author} | {self.book_id} | {self.genre} | {self.year} | {self.is_available}")
+    def __str__(self):
+        return f"{self.name} | {self.author} | {self.book_id} | {self.genre} | {self.year} | {self.is_available}"
+    
+    def __repr__(self):
+        return f"Book({self.name!r}, {self.author!r}, {self.year})"
 
     def toggle_status(self):
-        pass
+        self.is_available = not self.is_available
+        return "available" if self.is_available else "unavailable"
 
 lib = Library()
 book_1 = Book('Jack', 'Maxim', 1, 2025, 'fiction', True)
-lib.add_books(book_1)
+lib.add_book(book_1)
 
 book_2 = Book('Luck', 'Vadim', 2, 2023, 'history', True)
-lib.add_books(book_2)
+lib.add_book(book_2)
 
-book_3 = Book('Make', 'Vova', 3, 2024, 'Novel', True)
-lib.add_books(book_3)
+book_3 = Book('Make', 'Kola', 3, 2024, 'Novel', True)
+lib.add_book(book_3)
 
-book_4 = Book('Leak', 'Vova', 4, 2021, 'history', True)
-lib.add_books(book_4)
+book_4 = Book('Leak', 'Ola', 4, 2021, 'history', True)
+lib.add_book(book_4)
 
-book_5 = Book('Goal', 'Vova', 5, 2017, 'Novel', True)
-lib.add_books(book_5)
+book_5 = Book('Goal', 'Karina', 5, 2017, 'Novel', True)
+lib.add_book(book_5)
 
-book_6 = Book('Cant hurt me', 'Vova', 6, 2015, 'self improvement', True)
-lib.add_books(book_6)
+book_6 = Book('Cant hurt me', 'David', 6, 2015, 'self improvement', True)
+lib.add_book(book_6)
 
-book_7 = Book('The one', 'Vova', 7, 2020, 'Novel', True)
-lib.add_books(book_7)
+book_7 = Book('The one', 'John', 7, 2020, 'Novel', True)
+lib.add_book(book_7)
 
-lib.show_all_Books()
+print(book_7,'\n',book_6)
 
-user_1 = User('Maxim', 'Nakonechnuiy', 1)
-lib.add_users(user_1)
+user_1 = User('Maxim', 'Nakonechnuiy', 1, "sth")
+lib.add_user(user_1)
 
-user_2 = User('Vova', 'Nakonechnuiy', 2)
-lib.add_users(user_2)
+user_2 = User('Vova', 'Nakonechnuiy', 2, "sth")
+lib.add_user(user_2)
 
-user_3 = User('Vadim', 'Nakonechnuiy', 3)
-lib.add_users(user_3)
+user_3 = User('Vadim', 'Nakonechnuiy', 3, "sth")
+lib.add_user(user_3)
 
+for i in lib.users:
+    print(i)
 
+print()
+print("All books:")
+for i,book in enumerate(lib._books, start=1):
+    print(i, book)
+
+book_5.toggle_status()
+
+print()
+
+for s in user_1.borrowed_books:
+    print(s)
+print()
+lib.issue_book_to_user(3, 6)
 
